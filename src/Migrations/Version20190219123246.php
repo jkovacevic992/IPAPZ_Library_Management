70e247df563970dc75bc876f1eaec32345cef661
jkovacevic992 <jkovacevic992@gmail.com>
2019-02-19 15:22:05 +0100

    Fixed error when there's a duplicate email

---
+++ src/Migrations/Version20190219123246.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20190219123246 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE genre CHANGE name name VARCHAR(255) NOT NULL');
+        $this->addSql('ALTER TABLE user CHANGE first_name first_name VARCHAR(255) NOT NULL, CHANGE last_name last_name VARCHAR(255) NOT NULL');
+        $this->addSql('ALTER TABLE book CHANGE name name VARCHAR(255) NOT NULL, CHANGE author author VARCHAR(255) NOT NULL');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE book CHANGE name name LONGTEXT NOT NULL COLLATE utf8mb4_unicode_ci, CHANGE author author LONGTEXT NOT NULL COLLATE utf8mb4_unicode_ci');
+        $this->addSql('ALTER TABLE genre CHANGE name name LONGTEXT NOT NULL COLLATE utf8mb4_unicode_ci');
+        $this->addSql('ALTER TABLE user CHANGE first_name first_name LONGTEXT NOT NULL COLLATE utf8mb4_unicode_ci, CHANGE last_name last_name LONGTEXT NOT NULL COLLATE utf8mb4_unicode_ci');
+    }
+}
