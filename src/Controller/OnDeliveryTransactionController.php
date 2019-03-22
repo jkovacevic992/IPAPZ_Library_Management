fafec3e0f9cf09c2104771c5db3a01c43e6f8c57
jkovacevic992 <jkovacevic992@gmail.com>
2019-03-22 09:51:32 +0100

    Admin can now see invoice before charging the client

--- src/Controller/OnDeliveryTransactionController.php
+++ src/Controller/OnDeliveryTransactionController.php
@@ -135,4 +135,33 @@ class OnDeliveryTransactionController extends AbstractController
 
         return ['lateFee' => $lateFee, 'issueDate' => $issueDate, 'onDeliveryTransaction' => $onDeliveryTransaction];
     }
+
+    /**
+     * @Route("/employee/see_invoice/{id}", name="see_invoice")
+     * @param Borrowed $borrowed
+     * @throws \Exception
+     */
+    public function seeInvoice(Borrowed $borrowed)
+    {
+        $time = $borrowed->getReturnDate();
+        $timeDiff =date_diff(new \DateTime('now'), $time)->d;
+        $lateFee = sprintf("%.2f", 0.00);
+        if ($time < new \DateTime('now')) {
+            $lateFee = sprintf("%.2f", $timeDiff * 0.5 * count($borrowed->getBorrowedBooks()));
+        }
+        $pdfOptions = new Options();
+        $pdfOptions->set('defaultFont', 'Arial');
+        $domPdf = new Dompdf($pdfOptions);
+        $html = $this->renderView('pdf/invoice.html.twig', [
+            'title' => 'Something',
+            'borrowed' => $borrowed,
+            'lateFee' => $lateFee
+        ]);
+        $domPdf->loadHtml($html);
+        $domPdf->setPaper('A4', 'portrait');
+        $domPdf->render();
+        $domPdf->stream("mypdf.pdf", [
+            'Attachment' => false
+        ]);
+    }
 }
