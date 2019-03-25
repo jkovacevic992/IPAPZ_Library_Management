6343bf9b94ea27cf81912fcc5c83d6d16a929837
jkovacevic992 <jkovacevic992@gmail.com>
2019-03-25 15:59:11 +0100

    Show more/less genres now stays the same after refresh

--- public/js/showMore.js
+++ public/js/showMore.js
@@ -1,22 +1,71 @@
+
 $(function () {
-    $(".genre").slice(0, 5).show();
+    var cookie = getCookie('showMoreCookie');
+    if(cookie != 'clicked') {
+        $(".genre").slice(0, 5).show();
+        $("#loadMore").on('click', function (e) {
+            e.preventDefault();
 
-    $("#loadMore").on('click', function (e) {
-        e.preventDefault();
-        if($(".genre:hidden").length !== 0){
-            $(".genre:hidden").slideDown();
-            $("#loadMore").text("Show less");
-        }else{
+            if ($(".genre:hidden").length !== 0) {
+                $(".genre:hidden").slideDown();
+                $("#loadMore").text("Show Less");
+                createCookie('showMoreCookie','clicked',1);
 
-            $(".genre").slice(5,$(".genre").length).slideUp();
-            $("#loadMore").text("Show more");
+            } else {
 
-        }
-
-
-    });
+                $(".genre").slice(5, $(".genre").length).slideUp();
+                $("#loadMore").text("Show More");
+                createCookie('showMoreCookie','unclicked',1);
+            }
 
 
+        });
+    }else {
+        $(".genre").show();
+        $("#loadMore").text("Show Less");
+        $("#loadMore").on('click', function (e) {
+            e.preventDefault();
+            $(".genre").slice(5, $(".genre").length).slideUp();
+            $("#loadMore").text("Show More");
+            createCookie('showMoreCookie','unclicked',1);
+        });
 
+
+    }
 
 });
+
+
+function getCookie(name) {
+    var dc = document.cookie;
+    var prefix = name + "=";
+    var begin = dc.indexOf("; " + prefix);
+    if (begin == -1) {
+        begin = dc.indexOf(prefix);
+        if (begin != 0) return null;
+    }
+    else
+    {
+        begin += 2;
+        var end = document.cookie.indexOf(";", begin);
+        if (end == -1) {
+            end = dc.length;
+        }
+    }
+    // because unescape has been deprecated, replaced with decodeURI
+    //return unescape(dc.substring(begin + prefix.length, end));
+    return decodeURI(dc.substring(begin + prefix.length, end));
+}
+
+function createCookie(name,value,days) {
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime()+(days*24*60*60*1000));
+        var expires = "; expires="+date.toGMTString();
+    }
+    else var expires = "";
+    document.cookie = name+"="+value+expires+"; path=/";
+}
+
+
+
